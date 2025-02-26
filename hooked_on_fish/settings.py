30c35b1ca1146b0da41c3c34b0c7b87189a38bf2
Karlan Lemmen <karlan@MacBook-Pro-van-Karlan.local>
2025-02-26 10:22:07 +0000

    Conditionally setting DEBUG

--- hooked_on_fish/settings.py
+++ hooked_on_fish/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Explicitly set DEBUG to True
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
