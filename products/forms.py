92214344686051bf89a3b822e95c969498009be8
Karlan Lemmen <karlan@MacBook-Pro-van-Karlan.local>
2025-02-27 15:24:06 +0000

    Changed the Meta class to explicitly list the fields to prevent image field shown twice

--- products/forms.py
+++ products/forms.py
@@ -7,10 +7,10 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
-    
+        fields = ['name', 'description', 'price', 'image']
+
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
