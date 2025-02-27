3aceb0d92e04b86fb75e679e9b2b45764ddf651f
Karlan Lemmen <karlan@MacBook-Pro-van-Karlan.local>
2025-02-27 11:55:29 +0000

    Downgrade Python to 3.11 to fix SMTP issue

--- hooked_on_fish/settings.py
+++ hooked_on_fish/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Explicitly set DEBUG to True
-#DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
