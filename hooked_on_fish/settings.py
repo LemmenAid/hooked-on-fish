58f40cdce3bc3bb03c9be327961957eb71683b5d
Karlan Lemmen <karlan@MacBook-Pro-van-Karlan.local>
2025-02-27 11:51:18 +0000

    Debug for investigating sign up error on deployed site

--- hooked_on_fish/settings.py
+++ hooked_on_fish/settings.py
@@ -29,11 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Explicitly set DEBUG to True
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 '.herokuapp.com',]
+                 'hooked-on-fish-85d55f56e378.herokuapp.com',]
 
 
 # Application definition
@@ -149,13 +150,6 @@ else:
         }
     }
 
-#CSRF_TRUSTED_ORIGINS = [
-#    "https://*.codeinstitute-ide.net/",
-#    "https://*.herokuapp.com"
-#]
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -211,13 +205,13 @@ if not CLOUDINARY_URL:
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 20
-STRIPE_CURRENCY = 'eur'
+STRIPE_CURRENCY = "eur"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 
-if 'DEVELOPMENT' in os.environ:
+if "DEVELOPMENT" in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'hookedonfish@example.com'
 else:
@@ -228,8 +222,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-
 
 
 # Default primary key field type
